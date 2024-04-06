a737f01aea561ef779c5c2ae6bd579370b795b1e
SanguMyageri <61820792+Sangamesh-Myageri@users.noreply.github.com>
2024-04-06 12:57:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// Example: Add smooth scrolling for your nav links (Optional)
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
 document.querySelector('.nav-icon').addEventListener('click', function() {
     document.querySelector('.nav-links').classList.toggle('active');
 });
-
