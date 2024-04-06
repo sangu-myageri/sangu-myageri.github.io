fef4d8163f7deb9394a3e76dcc844411ad2ab86c
SanguMyageri <61820792+Sangamesh-Myageri@users.noreply.github.com>
2024-04-06 15:02:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// offcanvas-navbar js
+  (() => {
+  'use strict'
+
+  document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  })
+})()
 
