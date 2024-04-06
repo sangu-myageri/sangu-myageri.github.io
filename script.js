c48a30f00a5c850c4511f9f25369763166c5d7ee
SanguMyageri <61820792+Sangamesh-Myageri@users.noreply.github.com>
2024-04-06 13:24:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-document.querySelector('.nav-icon').addEventListener('click', function() {
-    document.querySelector('.nav-links').classList.toggle('active');
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.nav-icon').addEventListener('click', function () {
+        document.querySelector('.nav-links').classList.toggle('active');
+    });
 });
