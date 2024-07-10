0b088df8b52236a4219ac478b44a79c7e7f3d4d7
SanguMyageri <61820792+Sangamesh-Myageri@users.noreply.github.com>
2024-07-11 02:28:06 +0530

    Fixed menu in footer JS

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 $(document).ready(function () {
   var navbarBrand = $('.navbar-brand');
+  var lastScrollTop = 0;
+  var footerMenu = $('.navbar.fixed-bottom');
 
   $(window).on('scroll', function () {
-    if ($(window).scrollTop() > 50) { // Adjust the scroll value to your preference
+    var scrollTop = $(this).scrollTop();
+
+    // Handle navbar brand and job role visibility
+    if (scrollTop > 50) { // Adjust the scroll value to your preference
       navbarBrand.addClass('scrolled');
       $('.job-role').hide();
     } else {
       navbarBrand.removeClass('scrolled');
       $('.job-role').show();
     }
-  });
-  // var previousScroll = 0;
-  // var navbar = $('.navbar');
 
-  // $(window).on('scroll', function () {
-  //   var currentScroll = $(this).scrollTop();
-  //   if (currentScroll > previousScroll) {
-  //     // Scrolling down
-  //     navbar.addClass('navbar-hidden');
-  //   } else {
-  //     // Scrolling up
-  //     navbar.removeClass('navbar-hidden');
-  //   }
-  //   previousScroll = currentScroll;
-  // });
-
-  $('#navbarSideCollapse').on('click', function () {
-    $('.offcanvas-collapse').toggleClass('open');
+    // Handle footer menu visibility
+    if (scrollTop > lastScrollTop) {
+      footerMenu.addClass('hidden');
+    } else {
+      footerMenu.removeClass('hidden');
+    }
+    lastScrollTop = scrollTop;
   });
 });
-
